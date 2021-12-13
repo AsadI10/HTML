dfbe9720a4703bdc82ebebd7057ae3bb7b675fa1
31005646 <31005646@my.shu.ac.uk>
2021-12-13 20:07:17 +0000

    master

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 function myFunction() {
     alert("Thanks for registering your account");
   }
+  // $(".slidecontainer").slidecontainer({
+  //   item: 1,
+  //   slideMargin: 0,
+  //   loop: true,
+  //   });
+  // $(".main").click(function(){
+  //   // console.info("I was clicked!")
+  //   $(".main").slideToggle("fast")
+  //  })
