f817613c3e73ece8d8d6f93a9bc299691889c4a8
31005646 <31005646@my.shu.ac.uk>
2021-12-16 16:55:09 +0000

    master

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 function myFunction() {
-    alert("Thanks for registering your account");
-  }
-  function myFunction() {
     var x = document.getElementById("myPswrd");
     if (x.type === "password") {
       x.type = "text";
     } else {
       x.type = "password";
     }
+  }
+  function myFunction() {
+    alert("Thanks for registering your account");
   }
