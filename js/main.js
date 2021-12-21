c195f090b5e362cf63d431955b5aabef94fd7c7b
31005646 <31005646@my.shu.ac.uk>
2021-12-21 16:54:53 +0000

    master

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+(function(){
 function myFunction() {
     var x = document.getElementById("myPswrd");
     if (x.type === "password") {
@@ -9,3 +10,23 @@ function myFunction() {
   function myFunction() {
     alert("Thanks for registering your account");
   }
+var imageAr = ['website-images-assets/IMG_1005.jpeg',
+'website-images-assets/cantor-gallery.jpg',
+'website-images-assets/IMG_1439.jpeg' ]
+
+console.dir(imageAr)
+
+chgImage();
+setInterval(chgImage, 2000)
+var imgCounter = 0;
+function chgImage()
+{
+  document.getElementById("slide").setAttribute("src", imageAr[imgCounter]);
+  imgCounter++;
+  if (imgCounter === imageAr.length)
+  {
+    imgCounter = 0;
+  }
+}
+
+})()
