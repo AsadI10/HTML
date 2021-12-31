771a716b7c5eb7dee5055386a608bc0cc4d9641b
31005646 <31005646@my.shu.ac.uk>
2021-12-31 21:03:03 +0000

    master

---
+++ README.txt
@@ -0,0 +1,2 @@
+I haven't attempted the database/dynamic part. 
+As a bonus feature which I created in my HTML is that the user can click on show password and it will show the password.
--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 function myFun() {
   alert("Thanks for registering your account");
 }
-var imageAr = ['website-images-assets/cantor-gallery.jpg',
-'website-images-assets/IMG_1318.jpeg',
- 'website-images-assets/IMG_1005.jpeg' ]
+// var imageAr = ['website-images-assets/cantor-gallery.jpg',
+// 'website-images-assets/IMG_1318.jpeg',
+//  'website-images-assets/IMG_1005.jpeg' ]
 
-console.dir(imageAr)
+// console.dir(imageAr)
 
-chgImage();
-setInterval(chgImage, 2000)
-var imgCounter = 0;
-function chgImage()
-{
-  document.getElementById("slide").setAttribute("src", imageAr[imgCounter]);
-  imgCounter++;
-  if (imgCounter === imageAr.length)
-  {
-    imgCounter = 0;
-  }
-}
+// chgImage();
+// setInterval(chgImage, 2000)
+// var imgCounter = 0;
+// function chgImage()
+// {
+//   document.getElementById("slide").setAttribute("src", imageAr[imgCounter]);
+//   imgCounter++;
+//   if (imgCounter === imageAr.length)
+//   {
+//     imgCounter = 0;
+//   }
+// }
 function myFunction() {
     var x = document.getElementById("myPswrd");
     if (x.type === "password") {
@@ -30,3 +30,26 @@ function myFunction() {
   function init(){
     alert("Thanks for filling the form, we will get back to you!")
   }
+  var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("Sliides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
